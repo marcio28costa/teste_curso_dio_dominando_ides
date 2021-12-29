9794bf844075ac118bcd8b753d2168c972512488
=Marcio Costa <marcio28costa@hotmail.com>
2021-12-29 07:36:35 -0300

    anexando arquivos

---
+++ src/br/com/dio/first_prg.java
@@ -0,0 +1,15 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class first_prg {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("hello word");
+		
+		Gato gato = new Gato();
+		System.out.println(gato);
+	}
+
+}
